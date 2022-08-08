fce25841c1b7853f62689ca11462dee886793ed0
Nilcle Neres Modesto <nilcleneresmodesto@gmail.com>
2022-08-08 13:41:26 -0300

    Tentando

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+ 
+
+import TelaHome from "./Componentes/TelaHome";
+
+import TelaDescricao from "./Componentes/TelaDescricao";
+
+import TelaRotas from "./Componentes/TelaRotas";
+
+import TelaBiblioteca from "./Componentes/TelaBiblioteca";
+
+ 
+
+const Abas = createBottomTabNavigator();
+
+ 
+
+export default function App () {
+
+  return (
+
+    <NavigationContainer >
+
+      <Abas.Navigator>
+
+        <Abas.Screen name="Home" component = { TelaHome }/>
+
+        <Abas.Screen name="Telas" component = { TelaDescricao } />
+
+        <Abas.Screen name="Rotas" component = { TelaRotas } />
+
+        <Abas.Screen name="Biblioteca" component = { TelaBiblioteca } />
+
+      </Abas.Navigator>
+
+    </NavigationContainer>
+
+  )
+
+};
